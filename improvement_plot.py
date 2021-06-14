ee01870834e1677308e637803b1e07a9bc9bb670
marcinz99 <marcinzieba1999@gmail.com>
2021-06-14 11:40:54 +0200

    Plot script now shows the final mean and std

--- improvement_plot.py
+++ improvement_plot.py
@@ -34,3 +34,5 @@ if __name__ == "__main__":
         plt.grid(axis="y")
         plt.tight_layout()
         plt.savefig("lifespans.png")
+
+        print("Mean result: {:.3f} (std. {:.3f})".format(np.mean(np.array(ys), axis=0)[-1], np.std(np.array(ys)[-1])))
